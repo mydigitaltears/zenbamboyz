93688904222747d1acf02d0256babb42b740ac11
Xavier <xavier.touikan@gmail.com>
2020-01-08 18:39:26 -0500

    Updates

---
+++ precache-manifest.7606064c2dc083d0dc73303e0a94eae3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df384b18833536051f534074cdf1cfa",
+    "url": "/zenbamboyz/index.html"
+  },
+  {
+    "revision": "b4a542156a2db4a656da",
+    "url": "/zenbamboyz/static/css/main.f1527b62.chunk.css"
+  },
+  {
+    "revision": "51d098739f8f271b94b2",
+    "url": "/zenbamboyz/static/js/2.6ba8ddfe.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/zenbamboyz/static/js/2.6ba8ddfe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b4a542156a2db4a656da",
+    "url": "/zenbamboyz/static/js/main.3dc2b394.chunk.js"
+  },
+  {
+    "revision": "cebcafeb09e41e5b6415",
+    "url": "/zenbamboyz/static/js/runtime-main.8ec5bde0.js"
+  },
+  {
+    "revision": "753df9f760c45488a883d41db0953dc3",
+    "url": "/zenbamboyz/static/media/Agra Axera.753df9f7.ttf"
+  },
+  {
+    "revision": "7f33a1da9ef330939ba78c5d61b3e5f9",
+    "url": "/zenbamboyz/static/media/Lacquer-Regular.7f33a1da.ttf"
+  }
+]);
